7e3d7b80ecb909ad8474ef484c7825927ef3e28f
c8ler <104100704+c8ler@users.noreply.github.com>
2026-09-25 23:49:51 +0300

    Add files via upload

---
+++ levels/levels.js
@@ -0,0 +1,7 @@
+export const LEVELS = [
+ {id:1,key:'forest',name:'🌲 Лес',mood:'bright-day'},
+ {id:2,key:'lake',name:'🌊 Озеро',mood:'clear-water'},
+ {id:3,key:'village',name:'🏘️ Деревня',mood:'warm-evening'},
+ {id:4,key:'mountains',name:'⛰️ Горы',mood:'cold-dusk'},
+ {id:5,key:'lair',name:'👑 Кабанье логово',mood:'dark-moon'}
+];
